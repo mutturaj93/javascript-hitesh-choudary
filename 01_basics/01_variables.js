19e42bd15c0ea4969cf1fbf335f6e4e43cbda9b5
Mrityunjay Murgod <80122908+mutturaj93@users.noreply.github.com>
2023-03-25 06:20:51 +0000

    variables tutorial

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID =144553
+let accountEmail ="muttu@git.com"
+var accountPassword="hello"
+accountCity="Dandeli"
+let accountState
+
+/*
+Prefer not to use var
+because of funtional scope and block scope
+*/
+accountEmail="hdfc@yaoo"
+accountPassword="32423424"
+accountCity="Blr"
+//accountID=2 not allowed
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
